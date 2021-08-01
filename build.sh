7317eaa4431efaec514331dca1d44dc8f7aeaaa5
Carlos Nunez <carlosonunez@noreply.github.com>
2021-08-01 16:21:13 -0500

    Add support for simply monitoring for new versions

--- build.sh
+++ build.sh
@@ -2,21 +2,23 @@
 #vi: set ft=bash:
 if test -e "$(dirname "$0")/.env"
 then
-  export $(egrep -v '^#' "$(dirname "$0")/.env" | xargs -0)
+  # Quoting this will break it.
+  # shellcheck  disable=SC2046
+  export $(grep -Ev '^#' "$(dirname "$0")/.env" | xargs -0)
 fi
 
 DOCKER_HUB_USERNAME="${DOCKER_HUB_USERNAME?Please provide the username to Docker Hub.}"
 DOCKER_HUB_PASSWORD="${DOCKER_HUB_PASSWORD?Please provide the password to Docker Hub.}"
 DOCKER_HUB_REPO="${DOCKER_HUB_REPO:-$DOCKER_HUB_USERNAME/terraform}"
+# This was the first publicly-available version with ARM-compiled builds
 MIN_TERRAFORM_MAJOR_VERSION=0
-MIN_TERRAFORM_MINOR_VERSION=11
-MIN_TERRAFORM_PATCH_VERSION=15
-MIN_TERRAFORM_VERSION_SUPPORTED="${MIN_TERRAFORM_MAJOR_VERSION}.${MIN_TERRAFORM_MINOR_VERSION}.${MIN_TERRAFORM_PATCH_VERSION}"
+MIN_TERRAFORM_MINOR_VERSION=13
+MIN_TERRAFORM_PATCH_VERSION=5
 ONLY_SELECT_TF_STABLE_VERSIONS_RE='a href.*terraform_[0-9]{1}\.[0-9]{1,2}\.[0-9]{1,2}<'
 CAPTURE_VERSION_INSIDE_OF_HREF_RE='s/.*>terraform_(.*)<.*$/\1/'
 
 log_into_docker_hub_or_fail() {
-  if ! docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD"
+  if ! docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD" >/dev/null
   then
     >&2 echo "ERROR: Unable to log into Docker Hub; see logs for more details."
     exit 1
@@ -24,7 +26,7 @@ log_into_docker_hub_or_fail() {
 }
 
 get_terraform_versions() {
-  curl -H "Accept: application/json" https://releases.hashicorp.com/terraform/ | \
+  curl -s -H "Accept: application/json" https://releases.hashicorp.com/terraform/ | \
     grep -E "$ONLY_SELECT_TF_STABLE_VERSIONS_RE" | \
     sed -E "$CAPTURE_VERSION_INSIDE_OF_HREF_RE"
 }
@@ -43,8 +45,8 @@ version_is_supported() {
 }
 
 get_existing_docker_image_tags() {
-  curl "https://registry.hub.docker.com/v2/repositories/$DOCKER_HUB_REPO/tags?page_size=10000" | \
-    jq -r '.results[] | .name'
+  curl -s "https://registry.hub.docker.com/v2/repositories/$DOCKER_HUB_REPO/tags?page_size=10000" | \
+    jq -r '.results[] | select(.name | contains("-") | not) | .name'
 }
 
 image_already_exists() {
@@ -55,15 +57,8 @@ build_and_push_new_image() {
   _build() {
     docker build -t "$image_name" \
       --platform "$arch" \
-      --progress plain \
       --build-arg VERSION="$version" \
       --build-arg ARCH="$arch" .
-  }
-
-  _test() {
-    version="$(docker run --rm "$image_name" version)"
-    rc=$?
-    test "$rc" -eq 0 && ! test -z "$version"
   }
 
   _push() {
@@ -71,6 +66,7 @@ build_and_push_new_image() {
   }
 
   _push_linked_manifest() {
+    version="$1"
     unified_tag="$DOCKER_HUB_REPO:$version"
     docker manifest create "$unified_tag" \
       --amend "${unified_tag}-amd64" \
@@ -83,27 +79,40 @@ build_and_push_new_image() {
   do
     image_name="$DOCKER_HUB_REPO:${version}-${arch}"
     >&2 echo "INFO: Building Terraform v$version $arch"
-    if ! ( _build && _test && _push )
+    if ! ( _build && _push )
     then
       >&2 echo "ERROR: Failed to build and push version $version; stopping"
       exit 1
     fi
   done
 
-  _push_linked_manifest
+  _push_linked_manifest "$version"
 }
 
 log_into_docker_hub_or_fail
 existing_tags=$(get_existing_docker_image_tags)
+versions_needing_an_image=""
 while read -r version
 do
-  if ! version_is_supported "$version"
-  then
-    >&2 echo "ERROR: Version older than minimum supported version $MIN_TERRAFORM_VERSION_SUPPORTED: $version"
-    continue
-  fi
+  ! version_is_supported "$version" && continue
   if ! image_already_exists "$version" "$existing_tags"
   then
-    build_and_push_new_image "$version"
+    if test "$1" == "--alert-only"
+    then
+      versions_needing_an_image="$versions_needing_an_image,$version"
+    else
+      build_and_push_new_image "$version"
+    fi
+  else
+    >&2 echo "INFO: Docker image already exists for Terraform v$version"
   fi
 done <<< "$(get_terraform_versions)"
+
+# GitHub Actions doesn't support ARM runners, and spinning one up in AWS that does nothing
+# is a waste of money. Instead, alert me when a new version comes out so I can run this script.
+if ! test -z "$versions_needing_an_image"
+then
+  versions_fixed=$(echo "$versions_needing_an_image" | sed 's/^,//; s/,/, /g')
+  >&2 echo "INFO: Build Docker images for these Terraform versions: [$versions_fixed]"
+  exit 1
+fi
